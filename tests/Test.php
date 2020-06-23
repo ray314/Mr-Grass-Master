e1d97bf300907be7d0d247a230543b3e577e5d63
ray314 <yeungj1602@gmail.com>
2020-06-24 10:19:51 +1200

    Extend test class

--- tests/Test.php
+++ tests/Test.php
@@ -1,7 +1,7 @@
 <?php declare(strict_types=1);
 use PHPUnit\Framework\TestCase;
 
-final class Test {
+final class Test extends TestCase{
     function testCanBeUsedAsString(): void {
         $this->assertEquals('user@example.com','user@example.com');
     }
