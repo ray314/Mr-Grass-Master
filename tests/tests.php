cdfd65b5b1ce9969e8a0090aa6a801a8918d4259
James <44129935+james126@users.noreply.github.com>
2020-06-24 10:15:49 +1200

    Update tests.php

--- tests/tests.php
+++ tests/tests.php
@@ -1,4 +1,6 @@
 <?php
+declare(strict_types=1);
+use PHPUnit\Framework\TestCase;
 
     public function testCanBeUsedAsString(): void {
         $this->assertEquals('user@example.com','user@example.com');
